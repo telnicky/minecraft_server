26f31bef34cbc0bea65170870d49ddeb13d96d96
Travis Elnicky <telnicky@instructure.com>
2014-11-03 20:57:01 -0700

    added refresh

--- plugins/refresh.rb
+++ plugins/refresh.rb
@@ -1,11 +1,23 @@
+require "fileutils"
+
 class RefreshPlugin
   include Purugin::Plugin
   description 'refresh', 0.1
 
   def on_enable
-    player_command('refresh', 'pull down latest plugins from git', '/refresh') do |me, *|
-      me.msg "foobar"
- #     FileUtils.cp_r("../minecraft_plugins", "./plugins")
+    public_command('refresh', 'pull down latest plugins from git', '/refresh') do |me, *|
+      current_path = File.dirname(__FILE__)
+      path = File.join(current_path, "../../minecraft_plugins")
+
+      me.msg "refresh started"
+
+      `cd #{ path } && git pull`
+
+      Dir[path + "/*"].each do |file_path|
+        FileUtils.cp_r(file_path, current_path)
+      end
+
+      me.msg "refresh completed"
     end
   end
 end
