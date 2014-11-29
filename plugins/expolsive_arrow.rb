8ae24b9375d5e8b3e60a3eabb070e98d213d4910
Travis Elnicky <travis.elnicky@gmail.com>
2014-11-28 22:15:32 -0500

    blah

--- plugins/expolsive_arrow.rb
+++ plugins/expolsive_arrow.rb
@@ -3,16 +3,19 @@ class ExpolsiveArrowPlugin
   description 'explosive_arrow', 0.1
 
   def on_enable
+    enabled = false
     event(:projectile_hit) do |e|
-        shooter = e.entity.shooter
-        location = e.entity.location
-        world = location.world
-        distance = shooter.location.distance(location)
-        max_force = 50
-        force = max_force * (1 - (::Math::E ** -(distance/max_force)))
+      public_command('explosive_arrow', 'enable explosive arrow', '/explosive_arrow') { enabled = !enabled }
+      return unless enabled
+      shooter = e.entity.shooter
+      location = e.entity.location
+      world = location.world
+      distance = shooter.location.distance(location)
+      max_force = 50
+      force = max_force * (1 - (::Math::E ** -(distance/max_force)))
 
-        world.strike_lightning(location)
-        world.create_explosion(location, force)
+      world.strike_lightning(location)
+      world.create_explosion(location, force)
     end
   end
 end
--- plugins/house.rb
+++
@@ -1,9 +0,0 @@
-turtle('house') do 
-	square do
-		4.times do |i|
-			mark i
-			forward 5
-			turnleft 90
-		end
-	end
-end
