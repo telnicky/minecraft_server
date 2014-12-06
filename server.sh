6d77df6192cfbe10f205a57f3289ba7eb67aca4b
Travis Elnicky <telnicky@instructure.com>
2014-12-05 20:33:16 -0700

    added newest server

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  BINDIR=$(dirname "$(readlink -fn "$0")")
  cd "$BINDIR"
- java -Xms450M -Xmx1024M -jar craftbukkit.jar nogui
+ java -Xms450M -Xmx1024M -jar craftbukkit-1.7.9-R0.2.jar nogui
